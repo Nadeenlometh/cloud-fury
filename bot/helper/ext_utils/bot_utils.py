ef23dd6dc5be13485ec06a2e3439bfc32a0c67c4
Dawn India <shuvam.dawn12345@gmail.com>
2022-08-04 21:42:02 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -439,8 +439,11 @@ CPU:  {progress_bar(cpuUsage)} {cpuUsage}%
 RAM: {progress_bar(mem_p)} {mem_p}%
 DISK: {progress_bar(disk)} {disk}%
 TOTAL: {disk_t}GB | FREE: {disk_f}GB
+
 Working For: {currentTime}
 T-DL: {recv} | T-UL: {sent}
+
+Made with ❤️ by Dawn
 """
     return stats
 
